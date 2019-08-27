688e7cbaa357b78a4c1567fc7d22a42319f6e1e5
linhDevIOS <linhappcare@gmail.com>
2019-08-27 15:28:28 +0700

    Initial Commit

---
+++ MenuSide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MenuSide
+//
+//  Created by hoanglinh on 8/27/19.
+//  Copyright © 2019 hoanglinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
